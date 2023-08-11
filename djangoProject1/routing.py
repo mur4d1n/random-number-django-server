01ab3d1d55b888199c689f2aa4df25d6d79b5544
mur4d1n <mur4d1n@yandex.ru>
2023-08-11 22:29:42 +0300

    a

--- djangoProject1/routing.py
+++ djangoProject1/routing.py
@@ -1,13 +1,6 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from randNum.routing import websockets
-from channels.security.websocket import AllowedHostsOriginValidator
-from channels.auth import AuthMiddlewareStack
-
 
 application = ProtocolTypeRouter({
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(websockets)
-        )
-    )
+    "websocket": websockets
 })
